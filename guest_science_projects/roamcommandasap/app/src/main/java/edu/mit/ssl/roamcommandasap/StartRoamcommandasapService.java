87989e45e3924432c00e5d1bef1e13afecdb769a
albee <albee@mit.edu>
2021-02-20 15:28:21 -0500

    added guest science project from Ruben's generator script

---
+++ guest_science_projects/roamcommandasap/app/src/main/java/edu/mit/ssl/roamcommandasap/StartRoamcommandasapService.java
@@ -0,0 +1,107 @@
+
+/* Copyright (c) 2017, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ *
+ * All rights reserved.
+ *
+ * The Astrobee platform is licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package edu.mit.ssl.roamcommandasap;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import gov.nasa.arc.astrobee.android.gs.MessageType;
+import gov.nasa.arc.astrobee.android.gs.StartGuestScienceService;
+
+/**
+ * Class meant to handle commands from the Ground Data System and execute them in Astrobee
+ */
+
+public class StartRoamcommandasapService extends StartGuestScienceService {
+
+    // The API implementation
+    private ApiCommandImplementation api = null;
+
+     /**
+     * This function is called when the GS manager starts your apk.
+     * Put all of your start up code in here.
+     */
+    @Override
+    public void onGuestScienceStart() {
+        // Get a unique instance of the Astrobee API in order to command the robot.
+        api = ApiCommandImplementation.getInstance();
+
+        // Inform the GS Manager and the GDS that the app has been started.
+        sendStarted("info");
+    }
+
+    /**
+     * This function is called when the GS manager stops your apk.
+     * Put all of your clean up code in here. You should also call the terminate helper function
+     * at the very end of this function.
+     */
+    @Override
+    public void onGuestScienceStop() {
+        // Stop the API
+        api.shutdownFactory();
+
+        // Inform the GS manager and the GDS that this app stopped.
+        sendStopped("info");
+
+        // Destroy all connection with the GS Manager.
+        terminate();
+    }
+
+    /**
+     * This function is called when the GS manager sends a custom command to your apk.
+     * Please handle your commands in this function.
+     *
+     * @param command
+     */
+    @Override
+    public void onGuestScienceCustomCmd(String command) {
+        /* Inform the Guest Science Manager (GSM) and the Ground Data System (GDS)
+         * that this app received a command. */
+        sendReceivedCustomCommand("info");
+
+        try {
+            // Transform the String command into a JSON object so we can read it.
+            JSONObject jCommand = new JSONObject(command);
+
+            // Get the name of the command we received. See commands.xml files in res folder.
+            String sCommand = jCommand.getString("name");
+
+            // JSON object that will contain the data we will send back to the GSM and GDS
+            JSONObject jResult = new JSONObject();
+
+            switch (sCommand) {
+                // You may handle your commands here
+                default:
+                    // Inform GS Manager and GDS, then stop execution.
+                    jResult.put("Summary", new JSONObject()
+                        .put("Status", "ERROR")
+                        .put("Message", "Unrecognized command"));
+            }
+
+            // Send data to the GS manager to be shown on the Ground Data System.
+            sendData(MessageType.JSON, "data", jResult.toString());
+        } catch (JSONException e) {
+            // Send an error message to the GSM and GDS
+            sendData(MessageType.JSON, "data", "ERROR parsing JSON");
+        } catch (Exception ex) {
+            // Send an error message to the GSM and GDS
+            sendData(MessageType.JSON, "data", "Unrecognized ERROR");
+        }
+    }
+}
